c41f13c9dca81973e37b2a4c15d081b7f09fcb52
Allam <mohamed62hesham@gmail.com>
2023-12-15 23:00:40 +0200

    Added dependency injection

--- Main.java
+++ Main.java
@@ -1,25 +1,14 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Stack;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 public class Main {
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
         File file = new File("prefix.xml");
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-        Document doc = dBuilder.parse(file);
-        doc.getDocumentElement().normalize();
-        Node root = doc.getDocumentElement();
 
-        String expression = XML.traverseXML(root).trim();
+        String expression = XML.toString(file).trim();
         String[] tokens = expression.split(" ");
 
         Stack<Integer> evaluationStack = new Stack<>();
--- XML.java
+++ XML.java
@@ -1,14 +1,32 @@
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Stack;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 public class XML {
+    public static Node getRoot(File file) throws SAXException, IOException, ParserConfigurationException{
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(file);
+        doc.getDocumentElement().normalize();
 
-    static String traverseXML(Node root) {
+        return doc.getDocumentElement();
+    }
+
+    static String toString(File file) throws SAXException, IOException, ParserConfigurationException {
         StringBuilder result = new StringBuilder();
         Stack<Node> stack = new Stack<>();
-        stack.push(root);
+        stack.push(getRoot(file));
 
         while (!stack.isEmpty()) {
             Node node = stack.pop();
